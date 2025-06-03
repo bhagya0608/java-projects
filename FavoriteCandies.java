e046b9be9b351c4fbb64fbe26cbe3a12455d54e9
bhagya0608 <chittiboinabhagyalakshmi@gmail.com>
2025-06-03 15:40:49 +0530

    Add files via upload

---
+++ FavoriteCandies.java
@@ -0,0 +1,16 @@
+package javaproject;
+
+import java.util.LinkedHashSet;
+
+public class FavoriteCandies {
+
+	public static void main(String[] args) {
+		LinkedHashSet<String> candies = new LinkedHashSet<String>();
+		candies.add("Chocolate");
+		candies.add("Warwelon");
+		candies.add("Mango");
+		System.out.println(candies);
+	}
+}
+
+
