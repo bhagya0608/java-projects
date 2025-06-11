64f5aac64462e88bf9c3e13172a50fa02f277263
bhagya0608 <chittiboinabhagyalakshmi@gmail.com>
2025-06-11 11:45:38 +0530

    Add files via upload

---
+++ LinerTimeExample.java
@@ -0,0 +1,15 @@
+package xyz;
+
+public class LinerTimeExample {
+
+	public static void printAllElements(int[] arr) {
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("Element " + i + ":"+ arr[i]);
+		}
+	}
+		public static void main(String[] args) {
+			int[] numbers = {10,20,30,40,50};
+			printAllElements(numbers);
+		}
+	}
+	
